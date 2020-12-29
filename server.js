6749f2330abed816ccfdf724a4e7f54e22da23f9
Benito Gonzalez <ohnobenito@gmail.com>
2020-12-29 10:13:34 -0600

    Added functions for all possible tasks from to-do list, tested by console logging

--- server.js
+++ server.js
@@ -32,24 +32,66 @@ function start() {
         //if statements for each possible answer
         if (answer.toDo === "View all employees") {
             //insert function for viewing all employees
+            viewEmployees();
         } else if (answer.toDo === "View all employees by Department") {
-            //insert function for viewing all employees by dept
+             //insert function for viewing all employees by dept
+            viewDept();  
         } else if (answer.toDo === "View all employees by Manager") {
             //insert function for viewing all employees by manager
+            viewManager();
         } else if (answer.toDo === "Add Employee") {
             //insert function for adding employee
+            addEmployee();
         } else if (answer.toDo === "Remove Employee") {
             //insert function for removing employee
+            removeEmployee();
         } else if (answer.toDo === "Update Employee Role") {
             //insert function for updating employee role
+            updateRole();
         } else if (answer.toDo === "Update Employee Manager") {
             //insert function for updating employee manager
+            updateManager();
         } else if (answer.toDo === "Add Role") {
             //insert function for adding role
+            addRole();
         } else if (answer.toDo === "View All Roles") {
             //insert function for viewing all roles
+            viewRoles();
+
         } else {
             connection.end();
-        }
+            console.log("Bye Nina")
+        };
     })
 }
+
+function viewEmployees() {
+    console.log("View All Emps Test");
+};
+
+function viewDept() {
+    console.log("View All Emps by Department test");
+};
+
+function viewManager() {
+    console.log("View All Emps by Manager test");
+};
+
+function addEmployee() {
+    console.log("Add Employee Test");
+};
+function removeEmployee() {
+    console.log("Remove Emp Test");
+};
+function updateRole() {
+    console.log("Update Role Test");
+};
+function updateManager() {
+    console.log("Update Manager Test");
+};
+function addRole() {
+    console.log("Add Role Test");
+};
+function viewRoles() {
+    console.log("View All Roles Test")
+};
